134b3012060ad3161e29cd75ed656c4dc693058e
kirillkrohmal <krohmal_kirill@mail.ru>
2017-05-26 20:33:14 +0300

    change in Board

--- chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/BoardTest.java
@@ -16,12 +16,11 @@ public class BoardTest {
   @Test
     public void whenPaintBoardWithWidthThreeAndHeightThreeThenStringWithThreeColsAndThreeRows() {
         //assign
-		String expected = String.format("x x%s x %sx x%s", line, line, line);
-		final String line = System.getProperty("line.separator");
 		Board board = new Board();
+		final String line = System.getProperty("line.separator");
         //act
 		String result = board.paint(3, 3);
-        
+        String expected = String.format("x x%s x %sx x%s", line, line, line);
         //assert
         assertThat(result, is(expected));
 	}
@@ -30,11 +29,11 @@ public class BoardTest {
     @Test
     public void whenPaintBoardWithWidthFiveAndHeightFourThenStringWithFiveColsAndFourRows() {
         //assign
-		String expected = String.format("x x x%s x x %sx x x%s x x %s", line, line, line, line, line);
 		Board board = new Board();
 		final String line = System.getProperty("line.separator");
 		//act
 		String result = board.paint(5, 4);
+		String expected = String.format("x x x%s x x %sx x x%s x x %s", line, line, line, line, line);
 		//assert
 		assertThat(result, is(expected));
 		//напишите здесь тест, проверяющий формирование доски 5 на 4.
