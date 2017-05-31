ebd6181d4dd8b421885706d8508d3ad7b109d2d6
kirillkrohmal <krohmal_kirill@mail.ru>
2017-05-31 13:43:03 +0300

    change for Paint class

---
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -0,0 +1,30 @@
+package ru.job4j.loop;
+
+
+public class Paint {
+	public String piramid(int h) {
+		StringBuilder builder = new StringBuilder();
+		String line = System.getProperty("line.separator");
+			
+		int width = 2 * h - 1;
+		 for (int i = 1; i <= h; i++) {
+			for (int j = 1; j <= width; j++) {	
+				int part = width / 2;
+				int current = (2 * i - 1) / 2;
+				if (j <= part - current || j > part + current + 1) {
+					builder.append(" ");
+				} else {
+					builder.append("^");
+				}
+			}
+			builder.append(line);
+		}
+		return builder.toString();	
+		}
+	}
+
+
+		
+	
+
+
