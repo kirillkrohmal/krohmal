622643ca51cd626c39ce6129e1e8c04ca87cb008
kirillkrohmal <krohmal_kirill@mail.ru>
2017-06-01 17:25:45 +0300

    change RotateArray2

--- chapter_001/src/main/java/ru/job4j/array/RotateArray.java
+++ chapter_001/src/main/java/ru/job4j/array/RotateArray.java
@@ -2,20 +2,18 @@ package ru.job4j.array;
 
 
 public class RotateArray {
-	public int[][] rotate(int[][] array) {
-		
-				for(int i = 0; i < array.length;  i++){
-					for(int j = 0; j < array.length; j++){
-				
-						int temp = array[i][j];
-						array[i][j] = array[j][i];
-						array[j][i] = int temp;
-						}
-					}
-			return array;
+	public int[][] rotate(int[][] array) {	
+		for(int i = 0; i < array.length;  i++) {
+			for(int j = 0; j < array.length; j++) {
+					int temp = array[i][j];
+					array[i][j] = array[j][i];
+					array[j][i] = temp;
+				}
 			}
-		}
-		
+	return array;
+	}
+}
+
 		
 	
 	
