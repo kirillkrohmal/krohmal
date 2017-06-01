5195b33c41319b1ebb1cf31cf2bd2272ecc4801c
kirillkrohmal <krohmal_kirill@mail.ru>
2017-06-01 16:59:07 +0300

    change RotateArray

--- chapter_001/src/main/java/ru/job4j/array/RotateArray.java
+++ chapter_001/src/main/java/ru/job4j/array/RotateArray.java
@@ -3,13 +3,19 @@ package ru.job4j.array;
 
 public class RotateArray {
 	public int[][] rotate(int[][] array) {
-		for(int i = 0; i < array.length; i++){
-			for(int j = 0; j < array.length/4; j++){
-				int temp = array[i][array.length - i - 1];
-				array[i][array.length - i - 1] = array[array.length - 1][i];
-				array[array.length - 1][i] = temp;
+		
+				for(int i = 0; i < array.length;  i++){
+					for(int j = 0; j < array.length; j++){
+				
+						int temp = array[i][j];
+						array[i][j] = array[j][i];
+						array[j][i] = int temp;
+						}
+					}
+			return array;
 			}
 		}
-		return array;
-	}
-}
+		
+		
+	
+	
--- chapter_001/src/test/java/ru/job4j/array/RotateArrayTest.java
+++ chapter_001/src/test/java/ru/job4j/array/RotateArrayTest.java
@@ -6,21 +6,47 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class RotateArrayTest {
-    @Test
+/*    @Test
     public void whenRotateTwoRowTwoColArrayThenRotatedArray() {
         RotateArray rotateArray = new RotateArray();
-		int[][] array = new int[2][2];
+		int[][] array = 
+						{
+							{1, 2},
+							{3, 4}
+						};
 		int[][] result = rotateArray.rotate(array);
-		int[][] expected = new int[2][2];
+		int[][] expected = 
+						{
+							{3, 1},
+							{4, 2}
+						};
+						    1, 4, 7
+							2, 5, 8
+							3, 6, 9
+							
+							{7, 4, 1},
+							{8, 5, 2},
+							{9, 6, 3}
 		assertThat(result, is(expected));
     }
-
+*/
     @Test
     public void whenRotateThreeRowThreeColArrayThenRotatedArray() {
         RotateArray rotateArray = new RotateArray();
-		int[][] array = new int[3][3];
+		int[][] array = 
+						{
+							{1, 2, 3},
+							{4, 5, 6},
+							{7, 8, 9}
+						};
 		int[][] result = rotateArray.rotate(array);
-		int[][] expected = new int[3][3];
+		int[][] expected = 
+						{	
+							{1, 4, 7},
+							{2, 5, 8},
+							{3, 6, 9}
+						};
 		assertThat(result, is(expected));
-    }
+  
+  }
 }
