977f8961def17b9f7891673358f01ec625797b0b
kirillkrohmal <krohmal_kirill@mail.ru>
2017-06-07 20:35:43 +0300

    change in ArrayAdd class

---
+++ chapter_001/src/main/java/ru/job4j/arrayAdd/package-info.java
@@ -0,0 +1,12 @@
+/**
+* ArrayAdd.
+*/
+package ru.job4j.arrayAdd;
+
+/**
+* package-info for Class Calculator
+* @author Kirill Krohmal (mailto:krohmal_kirill@mail.ru)
+* @since 07.06.2017
+*/
+
+
--- chapter_001/src/test/java/ru/job4j/arrayAdd/ArrayAddTest.java
+++ chapter_001/src/test/java/ru/job4j/arrayAdd/ArrayAddTest.java
@@ -4,16 +4,23 @@ import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-
+/** 
+*Class ArrayAddTest
+*@author Krohmal Kirill (mailto:krohmal_kirill@mail.ru)
+*@since 07.06.2017
+*/
 
 public class ArrayAddTest {
     @Test
-	 public void whenSubIsEqualsOrigin() {
+	 public void whenArrayPlusAnotherArray() {
+		 //assign
 		ArrayAdd arrayAdd = new ArrayAdd();
 		int[] array1 = new int[]{1, 3};
 		int[] array2 = new int[]{2, 4};
+		//act
 		int[] result = arrayAdd.sumClass(array1, array2);
 		int[] expected = new int[]{1, 2, 3, 4};
+		//assert
 		assertThat(result,is(expected));
 	}
 }
