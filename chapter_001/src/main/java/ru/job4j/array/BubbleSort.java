11910913d5479a706b6f7efbe4e4c487dc3af7ad
kirillkrohmal <krohmal_kirill@mail.ru>
2017-06-10 12:41:17 +0300

    push origin master

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -5,7 +5,7 @@ public class BubbleSort{
 	public int[] sort(int[] array){
 		
 		for(int i = 0; i < array.length; i++){
-			for(int j = 1; j < array.length - i; j++){//от 1 до  элемента в массиве i-го элемента идет сокращение элементов которые перебраны
+			for(int j = 1; j < array.length; j++){//от 1 до  элемента в массиве i-го элемента идет сокращение элементов которые перебраны
 				if(array[j - 1] > array[j])//пока последний элемент больше первого элемента 
 				{
 					int temp = array[j - 1];//записываем в переменную temp последний элемент массива
--- chapter_001/src/main/java/ru/job4j/testTask/ArraySort.java
+++ chapter_001/src/main/java/ru/job4j/testTask/ArraySort.java
@@ -7,7 +7,7 @@ public class ArraySort {
 		boolean result = false;
 
 		for(int i = 0; i < array.length; i++) {
-			for(int j = 1; j < array.length - i; j++) {
+			for(int j = 1; j < array.length; j++){
 				if(array[j - 1] > array[j])
 				{
 					int temp = array[j - 1];
