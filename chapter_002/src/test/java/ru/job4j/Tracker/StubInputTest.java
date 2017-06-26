3b679853c43fa35ed5fba9a7808f1e90bb1b3fb8
kirillkrohmal <krohmal_kirill@mail.ru>
2017-06-26 15:41:41 +0300

    add new change in task StubInputTest

--- chapter_002/src/test/java/ru/job4j/Tracker/StubInputTest.java
+++ chapter_002/src/test/java/ru/job4j/Tracker/StubInputTest.java
@@ -22,53 +22,39 @@ public class StubInputTest {
     @Test
     public void whenShowAllItemsThenTrackerHasShowItemWithSameName() {
         Tracker tracker = new Tracker();//key, id, name, desc, creat
-        Input input = new StubInput(new String[]{"0", "key001", "1", "test name1", "desc1", "6"});
-        Input input1 = new StubInput(new String[]{"1", "key001", "1", "test name1", "desc1", "6"});
+        Input input = new StubInput(new String[]{"0", "key001", "1", "test name1", "desc1", "6", "1", "key001", "1", "test name1", "desc1", "6"});
         new StartUI(input, tracker).init();
-        new StartUI(input1, tracker).init();
         assertThat(tracker.findAll()[0].getName(), is("test name1"));
     }
     @Test
     public void whenUserFindByIdItemThenTrackerHasItemWithName() {
         Tracker tracker = new Tracker();//key, id, name, desc, creat
-        Input input = new StubInput(new String[]{"0", "key002", "2", "test name2", "desc2", "6"});
-        Input input1 = new StubInput(new String[]{"4", "key002", "2", "test name2", "desc2", "6"});
+        Input input = new StubInput(new String[]{"0", "key002", "2", "test name2", "desc2", "6", "4", "key002", "2", "test name2", "desc2", "6"});
         new StartUI(input, tracker).init();
-        new StartUI(input1, tracker).init();
         assertThat(tracker.findAll()[0].getName(), is("test name2"));
     }
 
     @Test
     public void whenUserDeleteItemThenTrackerHasNullItem() {
         Tracker tracker = new Tracker();//key, id, name, desc, creat
-        Input input = new StubInput(new String[]{"0", "key002", "2", "test name2", "desc2", "6"});
-        Input input2 = new StubInput(new String[]{"0", "key003", "3", "test name3", "desc3", "6"});
-        Input input1 = new StubInput(new String[]{"3", "key003", "3", "test name3", "desc3", "6"});
+        Input input = new StubInput(new String[]{"0", "key002", "2", "test name2", "desc2", "6", "0", "key003", "3", "test name3", "desc3", "6", "3", "key003", "3", "test name3", "desc3", "6"});
         new StartUI(input, tracker).init();
-        new StartUI(input2, tracker).init();
-        new StartUI(input1, tracker).init();
         assertThat(tracker.findAll()[0].getName(), is("test name2"));
     }
 
     @Test
     public void whenUserUpdateItemThenTrackerHasAnotherItem() {
         Tracker tracker = new Tracker();//key, id, name, desc, creat
-        Input input = new StubInput(new String[]{"0", "key004", "4", "test name4", "desc4", "6"});
-        Input input1 = new StubInput(new String[]{"0", "key006", "6", "test name6", "desc6", "6"});
-        Input input2 = new StubInput(new String[]{"2", "key006", "6", "test name6", "desc6", "6"});
+        Input input = new StubInput(new String[]{"0", "key004", "4", "test name4", "desc4", "6", "2", "key006", "6", "test name6", "desc6", "6"});
         new StartUI(input, tracker).init();
-        new StartUI(input1, tracker).init();
-        new StartUI(input2, tracker).init();
         assertThat(tracker.findAll()[0].getName(), is("test name6"));
     }
 
     @Test
     public void whenUserFindByNameItemThenTrackerHasItemWithName() {
         Tracker tracker = new Tracker();//key, id, name, desc, creat
-        Input input = new StubInput(new String[]{"0", "key005", "5", "test name5", "desc5", "6"});
-        Input input1 = new StubInput(new String[]{"5", "key005", "5", "test name5", "desc5", "6"});
+        Input input = new StubInput(new String[]{"0", "key005", "5", "test name5", "desc5", "6", "5", "key005", "5", "test name5", "desc5", "6"});
         new StartUI(input, tracker).init();
-        new StartUI(input1, tracker).init();
         assertThat(tracker.findAll()[0].getName(), is("test name5"));
     }
 }
