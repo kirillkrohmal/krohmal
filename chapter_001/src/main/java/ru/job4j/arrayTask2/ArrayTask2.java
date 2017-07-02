d22ab1aee4a385418977b0bf76889771bf591e37
kirillkrohmal <krohmal_kirill@mail.ru>
2017-07-02 13:51:37 +0300

    Сравним строку с подстрокой[#16799]

--- chapter_001/src/main/java/ru/job4j/arrayTask2/ArrayTask2.java
+++ chapter_001/src/main/java/ru/job4j/arrayTask2/ArrayTask2.java
@@ -10,14 +10,6 @@ public class ArrayTask2 {
 
         boolean result = false;
 
-
-
-        int subString = sub.length;
-        int originString = origin.length;
-
-        /*char[] subString = sub.toCharArray();
-        char[] originString = origin.toCharArray();*/
-
         for (int i = 0; i < origin.length; i++) {
             for (int j = 0; j < sub.length; j++) {
                 if (origin[i + j] == sub[i + j]) {
@@ -29,7 +21,6 @@ public class ArrayTask2 {
                     break;
             }
         }
-
         return result;
     }
 }
--- chapter_001/src/test/java/ru/job4j/arrayTask2/ArrayTaskTest2.java
+++ chapter_001/src/test/java/ru/job4j/arrayTask2/ArrayTaskTest2.java
@@ -13,8 +13,35 @@ public class ArrayTaskTest2 {
     @Test
     public void whenSubIsEqualsOrigin() {
         ArrayTask2 task = new ArrayTask2();
-        String[] sub = {"стиховедческий, стих, стиховед"};
+        String sub1 = "стиховедческий";
+        String sub2 = "стих";
+        String sub3 = "стиховед";
+        String[] sub = {sub1, sub2, sub3};
         String[] origin = {"стих"};
+        boolean result = task.contains(sub, origin);
+        boolean expected = true;
+        assertThat(result, is(expected));
+    }
+    @Test
+    public void whenSubIsEqualsOrigin1() {
+        ArrayTask2 task = new ArrayTask2();
+        String sub1 = "стихи";
+        String sub2 = "стихотворение";
+        String sub3 = "стиховед";
+        String[] sub = {sub1, sub2, sub3};
+        String[] origin = {"стих"};
+        boolean result = task.contains(sub, origin);
+        boolean expected = false;
+        assertThat(result, is(expected));
+    }
+    @Test
+    public void whenSubIsEqualsOrigin2() {
+        ArrayTask2 task = new ArrayTask2();
+        String sub1 = "стиховедческий";
+        String sub2 = "стишки";
+        String sub3 = "стишок";
+        String[] sub = {sub1, sub2, sub3};
+        String[] origin = {"стишок"};
         boolean result = task.contains(sub, origin);
         boolean expected = true;
         assertThat(result, is(expected));
