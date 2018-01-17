20ef35ef5d5d0609ee69e5fdbb10785d3abc4607
kirillkrohmal <krohmal_kirill@mail.ru>
2018-01-17 19:36:50 +0300

    Update: Реализовать собственную структуру данных - справочник.[#27853]

--- chapter_005/src/test/java/ru/job4j/GenericDirectoryTest/GenericDirectoryTest.java
+++ chapter_005/src/test/java/ru/job4j/GenericDirectoryTest/GenericDirectoryTest.java
@@ -1,11 +1,7 @@
 package ru.job4j.GenericDirectoryTest;
 
 import org.junit.Test;
-import ru.job4j.UserMap.OverLap;
-import ru.job4j.UserMap.User;
-
-import java.util.Calendar;
-import java.util.Map;
+import ru.job4j.GenericDirectory.GenericDirectory;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -17,22 +13,48 @@ import static org.junit.Assert.assertThat;
 public class GenericDirectoryTest {
     @Test
     public void iteratorTest() {
-        OverLap overLap = new OverLap();
-        Map<User, Object> result = overLap.map();
-        String[] expected =
-                {
-                        "User{name='Fedor', children=2, birthday=ru.job4j.UserMap.Calendar@deb6432} java.lang.Object@694f9431\n" + "User{name='Fedor', children=2, birthday=ru.job4j.UserMap.Calendar@deb6432} java.lang.Object@694f9431"
-                };
-        assertThat(result, is(expected));
+        GenericDirectory directory = new GenericDirectory();
+        //Map<User, Object> result = directory.map();
+        directory.insert(1, "Вася");
+        directory.insert(2, "Петя");
+        directory.insert(3, "Женя");
+        directory.insert(4, "Иван");
+        Object result = directory.get(1);
+        assertThat(result, is(true));
     }
     @Test
     public void iteratorTest2() {
-        User user1 = new User("Fedor", 2, Calendar.getInstance());
-        User user2 = new User("Fedor", 2, Calendar.getInstance());;
-        OverLap overLap = new OverLap();
-        Map<User, Object> result = overLap.map();
-        result.put(user1, "first");
-        result.put(user2, "first");
-        System.out.println(user1.equals(user2));
+        GenericDirectory directory = new GenericDirectory();
+        //Map<User, Object> result = directory.map();
+        directory.insert(1, "Вася");
+        directory.insert(2, "Петя");
+        directory.insert(3, "Женя");
+        directory.insert(4, "Иван");
+        directory.delete(1);
+        directory.delete(2);
+        directory.delete(3);
+        directory.delete(4);
+        Object result = directory.get(1);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void iteratorTest3() {
+        GenericDirectory directory = new GenericDirectory();
+        //Map<User, Object> result = directory.map();
+        directory.insert(1, "Вася");
+        directory.insert(2, "Петя");
+        directory.insert(3, "Женя");
+        directory.insert(4, "Иван");
+
+        assertThat(directory.iterator().hasNext(), is(true));
+        assertThat(directory.iterator().next(), is(1));
+        assertThat(directory.iterator().hasNext(), is(true));
+        assertThat(directory.iterator().next(), is(2));
+        assertThat(directory.iterator().hasNext(), is(true));
+        assertThat(directory.iterator().next(), is(3));
+        assertThat(directory.iterator().hasNext(), is(true));
+        assertThat(directory.iterator().next(), is(4));
+        assertThat(directory.iterator().hasNext(), is(true));
     }
 }
