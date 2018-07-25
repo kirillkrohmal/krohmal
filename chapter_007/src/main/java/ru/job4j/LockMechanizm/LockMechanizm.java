7f49b7c6a47fe4a3436a83bd407c9ba251cc1e1d
kirillkrohmal <krohmal_kirill@mail.ru>
2018-07-25 19:32:46 +0300

    Реализовать шаблон Producer Customer.[#27909]

--- chapter_007/src/main/java/ru/job4j/LockMechanizm/LockMechanizm.java
+++ chapter_007/src/main/java/ru/job4j/LockMechanizm/LockMechanizm.java
@@ -1,7 +1,53 @@
 package ru.job4j.LockMechanizm;
 
+
 /**
  * Created by Comp on 23.11.2017.
  */
 public class LockMechanizm {
+    private final Object lock = new Object();
+    private boolean blockIt = true;
+
+    private void lock() throws InterruptedException {
+        synchronized (lock) {
+            System.out.println("lock");
+            if (this.blockIt) {
+                lock.wait();
+            }
+            System.out.println("usefull work");
+        }
+    }
+
+    private boolean unlock(boolean b) {
+        synchronized (lock) {
+            System.out.println("unlock");
+            if (this.blockIt) {
+                lock.notify();
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        LockMechanizm lockMechanizm = new LockMechanizm();
+
+        new Thread () {
+            @Override
+            public void run() {
+                try {
+                    lockMechanizm.lock();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        new Thread () {
+            @Override
+            public void run() {
+                lockMechanizm.unlock(true);
+            }
+        }.start();
+    }
 }
+
--- chapter_007/src/main/java/ru/job4j/ProducerCustomer/Consumer.java
+++ chapter_007/src/main/java/ru/job4j/ProducerCustomer/Consumer.java
@@ -3,7 +3,7 @@ package ru.job4j.ProducerCustomer;
 /**
  * Created by Comp on 26.11.2017.
  */
-public class Customer implements Runnable {
+public class Consumer implements Runnable {
     private volatile boolean isPresent = false;
 
     @Override
