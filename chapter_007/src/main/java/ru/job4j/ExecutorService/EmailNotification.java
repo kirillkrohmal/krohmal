c570c86ea64c6e9aeaa4146a317de9fd48f1fb2b
kirillkrohmal <krohmal_kirill@mail.ru>
2018-08-07 15:15:41 +0300

    ExecutorService рассылка почты.[#63621]

--- chapter_007/src/main/java/ru/job4j/ExecutorService/EmailNotification.java
+++ chapter_007/src/main/java/ru/job4j/ExecutorService/EmailNotification.java
@@ -1,12 +1,37 @@
 package ru.job4j.ExecutorService;
 
-import ru.job4j.UserStorage.User;
+import ru.job4j.ExecutorService.User;
 
 /**
  * Created by Comp on 27.07.2018.
  */
 public class EmailNotification {
+    private Object body;
+    private Object subject;
+
+    public Object getBody() {
+        return body;
+    }
+
+    public void setBody(Object body) {
+        this.body = body;
+    }
+
+    public Object getSubject() {
+        return subject;
+    }
+
+    public void setSubject(Object subject) {
+        this.subject = subject;
+    }
+
     void emailTo(User user) {
+        subject =
+
+        body = user.getUsername();
+    }
+
+    public void send(String suject, String body, String email) {
 
     }
 }
