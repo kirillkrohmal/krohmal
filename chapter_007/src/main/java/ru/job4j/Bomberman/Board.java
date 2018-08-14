d6ab45fbe0edb496a43e4fb5d9f337fe6ec8d2be
kirillkrohmal <krohmal_kirill@mail.ru>
2018-08-14 14:59:46 +0300

    Update : Реализовать ThreadPool[#63619]

--- chapter_007/src/main/java/ru/job4j/Bomberman/Board.java
+++ chapter_007/src/main/java/ru/job4j/Bomberman/Board.java
@@ -1,7 +1,15 @@
 package ru.job4j.Bomberman;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by Comp on 11.08.2018.
  */
 public class Board {
+    ReentrantLock[][] board() {
+
+        return new ReentrantLock[0][];
+    }
+
+
 }
--- chapter_007/src/main/java/ru/job4j/ThreadPool/PoolThread.java
+++ chapter_007/src/main/java/ru/job4j/ThreadPool/PoolThread.java
@@ -7,30 +7,30 @@ import java.util.concurrent.BlockingQueue;
  */
 public class PoolThread extends Thread {
     private BlockingQueue taskQueue = null;
-    private boolean       isStopped = false;
+    private boolean isStopped = false;
 
-    public PoolThread(BlockingQueue queue){
+    public PoolThread(BlockingQueue queue) {
         taskQueue = queue;
     }
 
-    public void run(){
-        while(!isStopped()){
-            try{
+    public void run() {
+        while (!isStopped()) {
+            try {
                 //Runnable runnable = (Runnable) taskQueue.dequeue();
                 //runnable.run();
-            } catch(Exception e){
+            } catch (Exception e) {
                 //log or otherwise report exception,
                 //but keep pool thread alive.
             }
         }
     }
 
-    public synchronized void doStop(){
+    public synchronized void doStop() {
         isStopped = true;
         this.interrupt(); //break pool thread out of dequeue() call.
     }
 
-    public synchronized boolean isStopped(){
+    public synchronized boolean isStopped() {
         return isStopped;
     }
 }
