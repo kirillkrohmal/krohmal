757303b7d7915d1eaadc1c18026ae977af6f4975
kirillkrohmal <krohmal_kirill@mail.ru>
2018-08-17 14:40:13 +0300

    Update : Обеспечить остановку потребителя.[#66833]

--- chapter_007/src/test/java/ru/job4j/NonBlockingCacheTest/NonBlockingCacheTest.java
+++ chapter_007/src/test/java/ru/job4j/NonBlockingCacheTest/NonBlockingCacheTest.java
@@ -1,8 +1,13 @@
 package ru.job4j.NonBlockingCacheTest;
 
+import org.junit.Assert;
 import org.junit.Test;
 import ru.job4j.NonBlockingCache.Base;
 import ru.job4j.NonBlockingCache.NonBlockingCache;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.hamcrest.core.Is.is;
 
 /**
  * Created by Comp on 09.08.2018.
@@ -12,10 +17,22 @@ public class NonBlockingCacheTest {
     Base base;
 
     @Test
-    public void iteratorTest() {
-        nonBlockingCache.add(base);
-        //assertNull(nonBlockingCache.get(base));
+    public void iteratorTest() throws InterruptedException {
+        AtomicReference<Exception> ex = new AtomicReference<>();
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        throw new RuntimeException("Throw Exception in Thread");
+                    } catch (Exception e) {
+                        ex.set(e);
+                    }
+                }
+        );
+        thread.start();
+        thread.join();
+        Assert.assertThat(ex.get().getMessage(), is("Throw Exception in Thread"));
     }
+
 
     @Test
     public void iteratorTest3() {
