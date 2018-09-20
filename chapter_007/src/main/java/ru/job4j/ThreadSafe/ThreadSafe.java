f0ee24116e57c6d35ca43ba8399847761a8acc84
kirillkrohmal <krohmal_kirill@mail.ru>
2018-09-20 09:53:18 +0300

    Update: ThreadSafe динамический список[#69705]

--- chapter_007/src/main/java/ru/job4j/ThreadSafe/ThreadSafe.java
+++ chapter_007/src/main/java/ru/job4j/ThreadSafe/ThreadSafe.java
@@ -2,12 +2,24 @@ package ru.job4j.ThreadSafe;
 
 import ru.job4j.DynamicListThread.DynamicListThread.DynamicListThread;
 
+import java.util.Iterator;
+
 /**
  * Created by Comp on 04.09.2018.
  */
-public class ThreadSafe extends DynamicListThread {
-   /* @Override
+public class ThreadSafe<E> extends DynamicListThread {
+
+    private Object array;
+
+    @Override
     public synchronized Iterator<E> iterator() {
         return copy(this.array).iterator();
-    }*/
+
+    }
+
+
+
+    void copy () {
+        
+    }
 }
