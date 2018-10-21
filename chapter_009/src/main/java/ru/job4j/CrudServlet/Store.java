6c45ecd602fc5824ea044c8e5cd109ea6bd0f1e4
kirillkrohmal <krohmal_kirill@mail.ru>
2018-10-21 19:05:19 +0300

    changes

--- chapter_009/src/main/java/ru/job4j/CrudServlet/Store.java
+++ chapter_009/src/main/java/ru/job4j/CrudServlet/Store.java
@@ -1,8 +1,23 @@
 package ru.job4j.CrudServlet;
 
+import java.util.List;
+
 /**
  * Created by Comp on 19.10.2018.
  */
 
 public interface Store {
+
+   void add(User user);
+
+   void update(User user);
+
+   void delete(User user);
+
+   List<User> findAll();
+
+   User findById();
+
+   User findByLogin();
+
 }
--- chapter_009/src/main/java/ru/job4j/CrudServlet/UserStore.java
+++ chapter_009/src/main/java/ru/job4j/CrudServlet/UserStore.java
@@ -1,16 +1,49 @@
 package ru.job4j.CrudServlet;
 
+import java.util.List;
+
 /**
  * Created by Comp on 17.10.2018.
  */
-public class UserStore implements Store {
+
+class MemoryStore implements Store {
 
 
-    private final UserStore users = UserStore.getInstance();
+    public MemoryStore() {
+    }
 
+    public static MemoryStore getInstance() {
 
-    public static UserStore getInstance() {
+        return null;
+    }
 
+    @Override
+    public void add(User user) {
+
+    }
+
+    @Override
+    public void update(User user) {
+
+    }
+
+    @Override
+    public void delete(User user) {
+
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public User findById() {
+        return null;
+    }
+
+    @Override
+    public User findByLogin() {
         return null;
     }
 }
