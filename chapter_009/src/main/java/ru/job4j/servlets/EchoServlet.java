b37608694ae4fc50aa52d50faa1f63d34660e49f
kirillkrohmal <krohmal_kirill@mail.ru>
2019-01-21 16:52:38 +0300

    Создать web - проект [#107033]

--- chapter_009/src/main/java/ru/job4j/servlets/EchoServlet.java
+++ chapter_009/src/main/java/ru/job4j/servlets/EchoServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 
 /**
@@ -17,6 +19,10 @@ import java.io.PrintWriter;
 public class EchoServlet extends HttpServlet {
 
     private static final Logger log = LoggerFactory.getLogger(EchoServlet.class);
+
+
+    private List<String> users = new CopyOnWriteArrayList<>();
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -70,6 +76,13 @@ public class EchoServlet extends HttpServlet {
                 "</html>"
         );
         writer.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        this.users.add(req.getParameter("login"));
+        doGet(req, resp);
     }
 }
 
