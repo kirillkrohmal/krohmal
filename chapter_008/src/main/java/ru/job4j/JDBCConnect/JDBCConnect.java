59097207bccac11b017d81a5ff755512968f2512
kirillkrohmal <krohmal_kirill@mail.ru>
2019-10-04 16:51:56 +0300

    Log4j 2. Логирование системы.[#114892]

---
+++ chapter_008/src/main/java/ru/job4j/JDBCConnect/JDBCConnect.java
@@ -0,0 +1,16 @@
+package JDBCConnect;
+
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+/**
+ * Created by Comp on 15.04.2019.
+ */
+public class JDBCConnect {
+    private static final Logger LOG = LogManager.getLogger(JDBCConnect.class.getName());
+
+    public static void main(String[] args) {
+
+
+    }
+}
