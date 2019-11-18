9e1e45a7cea5352ddbb5b2a4778ad530a9d16fd0
kirillkrohmal <krohmal_kirill@mail.ru>
2019-11-18 14:58:57 +0300

    annotations4

--- chapter_001/src/main/java/ru/job4j/task/package-info.java
+++ chapter_001/src/main/java/ru/job4j/task/package-info.java
@@ -1,10 +1,10 @@
 /**
-* task.
+* Task.
 */
 package ru.job4j.Task;
 
 /**
-* package-info for Class task
+* package-info for Class Task
 * @author Kirill Krohmal (mailto:krohmal_kirill@mail.ru)
 * @since 17.11.2019
 */
