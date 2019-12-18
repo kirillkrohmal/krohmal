0c7b5de6ce54bf0c20a0b83c68703db09ea3a2cb
kirillkrohmal <krohmal_kirill@mail.ru>
2019-12-18 17:03:30 +0300

    Liquibase Интеграционные тесты. Proxy.[#196964]

--- chapter_008/src/main/java/ru/job4j/storesql/StoreSQL.java
+++ chapter_008/src/main/java/ru/job4j/storesql/StoreSQL.java
@@ -15,7 +15,7 @@ public class StoreSQL implements AutoCloseable {
 
     public void generate(int size) {
         for (int i = 0; i < size; i++) {
-            String s1 = "INSERT INTO test(i) VALUES (?)";
+            String s1 = "INSERT INTO entry(i) VALUES (?)";
 
             try (PreparedStatement statement = connect.prepareStatement(s1);) {
                 statement.setString(1, s1);
@@ -27,7 +27,7 @@ public class StoreSQL implements AutoCloseable {
     }
 
     public String findAll() {
-        String s1 = "SELECT size FROM test";
+        String s1 = "SELECT size FROM entry";
 
         try (PreparedStatement statement = connect.prepareStatement(s1);) {
             statement.setString(1, s1);
@@ -41,19 +41,33 @@ public class StoreSQL implements AutoCloseable {
         return s1;
     }
 
-    public static void createNewDatabase(String fileName) {
+    public static void createNewDatabase(String fileName) throws SQLException {
 
         String url = "jdbc:sqlite:C:/sqlite/db/" + fileName;
 
-        try (Connection conn = DriverManager.getConnection(url)) {
+        Connection conn = DriverManager.getConnection(url);
+        Statement statement = conn.createStatement();
+
+        try {
+            String sql = "CREATE TABLE entry " + "(field INTEGER not NULL)";
+
+            statement.executeUpdate(sql);
+            System.out.println("Table successfully created...");
+
             if (conn != null) {
                 DatabaseMetaData meta = conn.getMetaData();
                 System.out.println("The driver name is " + meta.getDriverName());
                 System.out.println("A new database has been created.");
             }
-
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+        } finally {
+            if (statement != null) {
+                statement.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
         }
     }
 
@@ -69,6 +83,10 @@ public class StoreSQL implements AutoCloseable {
     }
 
     public static void main(String[] args) {
-        createNewDatabase("test.db");
+        try {
+            createNewDatabase("test.db");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
