c391aef519aacf70b75466fc8cb494cf5ea464f3
kirillkrohmal <krohmal_kirill@mail.ru>
2019-12-22 15:57:59 +0300

    Liquibase. Интеграционные тесты. Proxy.[#196964]

--- chapter_008/src/main/java/ru/job4j/integrationsql/Item.java
+++ chapter_008/src/main/java/ru/job4j/integrationsql/Item.java
@@ -87,18 +87,23 @@ public class Item {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         Item item = (Item) o;
-        return creat == item.creat && Objects.equals(id, item.id) && Objects.equals(key, item.key) && Objects.equals(name, item.name) && Objects.equals(description, item.description);
+        return creat == item.creat &&
+                Objects.equals(id, item.id) &&
+                Objects.equals(key, item.key) &&
+                Objects.equals(name, item.name) &&
+                Objects.equals(description, item.description);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(id, key, name, creat, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" + "id='" + id + '\'' + ", key='" + key + '\'' + ", name='" + name + '\'' + ", creat=" + creat + ", description='" + description + '\'' + '}';
     }
 }
