ad104aae506b6672dbcde3ca07a67b4700da5b5e
kirillkrohmal <krohmal_kirill@mail.ru>
2020-02-03 12:31:25 +0300

    Ajax Jquery[#231853]

---
+++ chapter_009/ajaxservlet/src/main/java/ru/job4j/servlet/AjaxServlet.java
@@ -0,0 +1,29 @@
+package ru.job4j.servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class AjaxServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("views/ViewUsers.jsp");
+        requestDispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter printWriter = new PrintWriter(resp.getOutputStream());
+
+        printWriter.flush();
+    }
+}
