73e588cf20c5df80209ee1bad4cf2d5cd47903de
kirillkrohmal <krohmal_kirill@mail.ru>
2021-05-24 17:04:48 +0300

    first commit

---
+++ chapter_011/finishspring/src/main/java/ru/job4j/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package ru.job4j.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository {
+}
+
+
