9256074a64bb377c754c1d2fa0bbeb744b4670c6
kirillkrohmal <krohmal_kirill@mail.ru>
2023-02-10 21:14:47 +0300

    first commit

--- chapter_001/src/main/java/ru/job4j/multiplicationloop/MultiplicationLoop.java
+++ chapter_001/src/main/java/ru/job4j/multiplicationloop/MultiplicationLoop.java
@@ -2,14 +2,11 @@ package ru.job4j.multiplicationloop;
 
 public class MultiplicationLoop {
     public static int mult(int a, int b) {
-        int f = 0;
-        for (int i = 0; i < a; i++) {
-            for (int j = 0; j < b; j++) {
-                if (i < j) {
-                    f *= i;
-                }
-            }
+        int f = 1;
+        for (int i = a; i <= b; i++) {
+            f = f * i;
         }
+
         return f;
     }
 }
--- chapter_001/src/main/java/ru/job4j/oddcount/OddCount.java
+++ chapter_001/src/main/java/ru/job4j/oddcount/OddCount.java
@@ -2,15 +2,12 @@ package ru.job4j.oddcount;
 
 public class OddCount {
     public static int count(int a, int b) {
-        int f = 0;
-        for (int i = 0; i < b; i++) {
-            for (int j = i; j < a; j++) {
-                if (i % 1 == 0 || j % 1 == 2) {
-                    f = i++;
-                    f = j++;
-                }
-            }
+        int count = 0;
+        for (int i = a; i <= b; i++) {
+            if (i % 2 != 0)
+                count++;
         }
-        return f;
+
+        return count;
     }
 }
